7a626f2d031853ff76c22a2dc8008862d0a9fbf8
Reid Pritchard <reidpritchard99@gmail.com>
2025-08-31 18:51:11 -0600

    fix: removed reference to process in config file

--- apps/client/src/config.ts
+++ apps/client/src/config.ts
@@ -1,22 +1,36 @@
 import { SETTINGS } from "@oer/configuration";
 
 // Get env type
-const env = process.env.NODE_ENV;
+let env: string;
+let PROD_URL: string;
+let PROD_PORT: number | string;
 
-const PROD_URL = process.env.WS_URL;
-const PROD_PORT = process.env.PORT || SETTINGS.SERVER_PORT || 8000;
+if (typeof process === "undefined" || !process.env) {
+  console.warn(
+    "process.env is undefined. Defaulting to development environment.",
+  );
+  env = "development";
+
+  PROD_URL = "ws://localhost";
+  PROD_PORT = 8000;
+} else {
+  env = process.env.NODE_ENV || "development";
+
+  PROD_URL = process.env.WS_URL || SETTINGS.SERVER_WS_URL || "ws://localhost";
+  PROD_PORT = process.env.PORT || SETTINGS.SERVER_PORT || 8000;
+}
 
 export const config = {
-	serverUrl:
-		env !== "production" ? SETTINGS.SERVER_WS_URL : `${PROD_URL}:${PROD_PORT}`,
+  serverUrl:
+    env !== "production" ? SETTINGS.SERVER_WS_URL : `${PROD_URL}:${PROD_PORT}`,
 
-	localStoragePlayerNameKey: "playerName",
-	localStoragePlayerSettingsKey: "playerSettings",
+  localStoragePlayerNameKey: "playerName",
+  localStoragePlayerSettingsKey: "playerSettings",
 
-	game: {
-		bottomCardDisplayDuration: 3000,
-	},
-	animation: {
-		cardPlayedAnimationDuration: 0.5,
-	},
+  game: {
+    bottomCardDisplayDuration: 3000,
+  },
+  animation: {
+    cardPlayedAnimationDuration: 0.5,
+  },
 };
